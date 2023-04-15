04b81a85fe445de0291be003464beaeb622f90e5
Leif Schelin <scleif@student.chalmers.se>
2023-04-15 19:25:23 +0200

    Now downloads chapters in parallel

--- parser.py
+++ parser.py
@@ -1,14 +1,16 @@
+import itertools
 import re
 import requests
+import threading
 
 from html.parser import HTMLParser
 
 ROOT_URL = "https://www.wordproject.org/"
 
-RE_BOOK_URL = re.compile('bibles/([a-z_]+)/index.htm')
-assert RE_BOOK_URL.match('bibles/no/index.htm')
+RE_TRANSLATION_URL = re.compile('bibles/([a-z_]+)/index.htm')
+assert RE_TRANSLATION_URL.match('bibles/no/index.htm')
 
-RE_SUB_BOOK_URL = re.compile('^[0-9]+/[0-9]+.htm')
+RE_BOOK_URL = re.compile('^[0-9]+/[0-9]+.htm')
 
 RE_WHITESPACE_ONLY = re.compile('^\\s+$')
 assert RE_WHITESPACE_ONLY.match('\r\n  ')
@@ -22,7 +24,7 @@ class Accumulator(HTMLParser):
     def run(self):
         print(f'downloading {self.root_url}')
         web_root = requests.get(self.root_url)
-        print('success')
+        # print('success')
 
         # Note: web_root.encoding says ISO-8859-1, but that's wrong.
         s = web_root.content.decode('utf-8')
@@ -42,7 +44,7 @@ class RootParser(Accumulator):
             self.handle_url(d['href'])
 
     def handle_url(self, url):
-        if RE_BOOK_URL.match(url):
+        if RE_TRANSLATION_URL.match(url):
             full_url = self.root_url + url
             # print("Encountered link:", full_url)
             self.result.append(full_url)
@@ -55,7 +57,7 @@ class BookParser(Accumulator):
             self.handle_url(d['href'])
 
     def handle_url(self, url):
-        if RE_SUB_BOOK_URL.match(url):
+        if RE_BOOK_URL.match(url):
             full_url = self.root_url.replace('index.htm', url)
             # print("Encountered sub book link:", full_url)
             self.result.append(full_url)
@@ -69,7 +71,7 @@ class ChapterParser(Accumulator):
 
     def handle_url(self, url):
         full_url = self.root_url.replace('1.htm', url)
-        print("Encountered chapter link:", full_url)
+        # print("Encountered chapter link:", full_url)
         self.result.append(full_url)
 
 
@@ -93,26 +95,90 @@ class VerseParser(Accumulator):
             self.callback_fn(data)
 
 
+class MyThread(threading.Thread):
+    def __init__(self, task_fn):
+        threading.Thread.__init__(self)
+        self.task_fn = task_fn
+        self.result = None
+
+    def run(self):
+        self.result = self.task_fn()
+
+class ThreadBundle():
+    def __init__(self):
+        self.threads = []
+
+    def AddTask(self, task_fn):
+        t = MyThread(task_fn)
+        self.threads.append(t)
+        t.start()
+
+    def AddMany(self, xs, task_fn):
+        for x in xs:
+            self.AddTask(lambda: task_fn(x))
+
+    def Join(self):
+        results = []
+        for t in self.threads:
+            t.join()
+            results.append(t.result)
+        return results
+
+
+def download_chapter(chapter_url):
+    result = []
+    VerseParser(chapter_url, lambda x: result.append(x)).run()
+    return result
+
+
 def download_book(book_url):
-    m = RE_BOOK_URL.search(book_url)
+    # Chapter 1
+    chapter_urls = [book_url]
+    # Fetch the other chapters:
+    chapter_urls += ChapterParser(book_url).run()
+
+    bundle = ThreadBundle()
+    bundle.AddMany(chapter_urls, download_chapter)
+    return bundle.Join()
+
+
+def download_translation(translation_url):
+    m = RE_TRANSLATION_URL.search(translation_url)
+    if not m:
+        print('ERROR: no match for ', translation_url)
+        return False
     translation = m.group(1)
-    found_verse = False
+
+    book_urls = BookParser(translation_url).run()
+    if not book_urls:
+        print('ERROR: no books found for', translation_url)
+        return False
+
+    found = False
     with open(f'data/{translation}.txt', 'w', encoding='utf-8') as out:
-        for book_url in BookParser(book_url).run():
-            for chapter_url in ChapterParser(book_url).run():
-                VerseParser(chapter_url, out.write).run()
-                found_verse = True
-    return found_verse
+        for book_url in book_urls:
+            ls = download_book(book_url)
+            if not ls:
+                print('ERROR: no data found for', book_url)
+                return False
+
+            content = ''.join(itertools.chain.from_iterable(ls))
+            out.write(content)
+            found = True
+    return found
 
 
 def download_all():
     for translation in RootParser(ROOT_URL).run():
-        found = download_book(translation)
+        found = download_translation(translation)
         if found:
             return # TODO get all books
 
-
 def main():
     download_all()
+
+    # bundle = ThreadBundle()
+    # bundle.AddMany([1,2,3,4,5,6,7], lambda x: x**2)
+    # print(bundle.Join())
 
 main()
